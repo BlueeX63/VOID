6f177e901f74bc1eb44b24ccdb35f10a1ea93036
BlueeX63 <bhavitraj@gmail.com>
2026-06-12 17:32:33 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "*",
+    origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, "http://localhost:5173"] : "http://localhost:5173",
   },
 });
 
